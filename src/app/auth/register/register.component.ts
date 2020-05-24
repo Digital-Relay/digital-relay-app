1d0c9c375eac45b855a919918b0a09c027148a39
MatusPilnan <m.pilnan@gmail.com>
2020-05-24 15:04:14 +0200

    Revert "remove initial API connection service"
    This reverts commit 5afbb0b7

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {AuthApiService} from '../auth-api.service';
+import {login} from '../../store/actions/auth.actions';
 import {Store} from '@ngrx/store';
 import {DigitalRelayState} from '../../store';
 
@@ -10,10 +12,11 @@ import {DigitalRelayState} from '../../store';
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
-  errorMessage: string | null;
+  errorMessage: String | null;
 
   constructor(
     private formBuilder: FormBuilder,
+    private authApiService: AuthApiService,
     private store: Store<DigitalRelayState>) {
     this.registerForm = this.formBuilder.group({
       registration: []
@@ -25,6 +28,14 @@ export class RegisterComponent implements OnInit {
 
   onSubmit() {
     console.log(this.registerForm.value);
+    const email = this.registerForm.value.registration.emailField;
+    const name = this.registerForm.value.registration.nameField;
+    const password = this.registerForm.value.registration.passwordField;
+    this.authApiService.register(email, name, password).toPromise().then(() => {
+      this.store.dispatch(login({email: email, password: password}));
+    }).catch((error) => {
+      this.errorMessage = error.error.response.errors[Object.keys(error.error.response.errors)[0]][0];
+    });
   }
 }
 
