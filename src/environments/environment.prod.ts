4f4f07e8951fc6fc11c129757cb43d1e14f6f757
MatusPilnan <m.pilnan@gmail.com>
2020-06-04 14:43:51 +0200

    fixes before

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 export const environment = {
-  production: false,
-  apiBaseUrl: window["env"]["apiUrl"] || "default",
-  debug: window["env"]["debug"] || false
+  production: true,
+  apiBaseUrl: window['env']['apiUrl'] || 'default',
+  debug: window['env']['debug'] || false
 };
